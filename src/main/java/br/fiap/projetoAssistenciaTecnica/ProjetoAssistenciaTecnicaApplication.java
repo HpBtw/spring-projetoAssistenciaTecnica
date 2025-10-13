1c3721c1963b596bf21153bf2ca730080fd3565a
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-13 19:58:24 -0300

    First commit

---
+++ src/main/java/br/fiap/projetoAssistenciaTecnica/ProjetoAssistenciaTecnicaApplication.java
@@ -0,0 +1,13 @@
+package br.fiap.projetoAssistenciaTecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAssistenciaTecnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAssistenciaTecnicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/fiap/projetoAssistenciaTecnica/ProjetoAssistenciaTecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fiap.projetoAssistenciaTecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAssistenciaTecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
